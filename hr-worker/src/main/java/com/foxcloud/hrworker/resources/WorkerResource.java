1e284c4e5c52eacb847d702586f85671fbe49823
José Wilson Junior <jwdevelopper@gmail.com>
2022-01-17 11:38:25 -0300

    fim da implementação da api worker

--- hr-worker/src/main/java/com/foxcloud/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/foxcloud/hrworker/resources/WorkerResource.java
@@ -1,9 +1,15 @@
 package com.foxcloud.hrworker.resources;
 
+import com.foxcloud.hrworker.entities.Worker;
 import com.foxcloud.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
@@ -11,4 +17,16 @@ public class WorkerResource {
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker worker = repository.findById(id).get();
+        return ResponseEntity.ok(worker);
+    }
 }
