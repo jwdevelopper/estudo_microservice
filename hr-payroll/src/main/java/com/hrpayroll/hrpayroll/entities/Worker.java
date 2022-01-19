a8ce3c73019a2ba3c84fdd1a02410f960b3f4c84
José Wilson Junior <jwdevelopper@gmail.com>
2022-01-19 12:17:01 -0300

    Impl RestTemplate

---
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/entities/Worker.java
@@ -0,0 +1,32 @@
+package com.hrpayroll.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- hr-payroll/src/main/java/com/hrpayroll/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/services/PaymentService.java
@@ -1,10 +1,14 @@
 package com.hrpayroll.hrpayroll.services;
 
 import com.hrpayroll.hrpayroll.entities.Payment;
+import com.hrpayroll.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
@@ -16,6 +20,9 @@ public class PaymentService {
     private RestTemplate restTemplate;
 
     public Payment getPayment(Long workerId, int days) {
-        return new Payment("Junin", 200.0, days);
+        Map<String,String> uriVariables = new HashMap<>();
+        uriVariables.put("id",workerId.toString());
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
